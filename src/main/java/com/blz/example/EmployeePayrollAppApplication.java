f082a8491fdbddc687d8120368b534dddc7fb71b
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-12-20 15:54:53 +0530

    Added:Code to create Employee Payroll App.

---
+++ src/main/java/com/blz/example/EmployeePayrollAppApplication.java
@@ -0,0 +1,12 @@
+package com.blz.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+	}
+}
