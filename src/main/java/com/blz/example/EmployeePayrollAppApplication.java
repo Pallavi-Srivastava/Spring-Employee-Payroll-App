1f98091528b8128ea95d8580055a86dd4c874c3b
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-12-22 17:21:30 +0530

    Added:Database setting as environment variable.

---
+++ src/main/java/com/blz/example/EmployeePayrollAppApplication.java
@@ -0,0 +1,16 @@
+package com.blz.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class EmployeePayrollAppApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context= SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		log.info("Employee Payroll App started in {} environment",context.getEnvironment().getProperty("environment"));
+	}
+}
