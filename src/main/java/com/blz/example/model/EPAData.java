aff9a6c81519a892b5357e38c1f7d90e29ead018
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-12-22 14:45:25 +0530

    Added:Code to determine Logging Levels,based on the application running in dev or production.

---
+++ src/main/java/com/blz/example/model/EPAData.java
@@ -0,0 +1,31 @@
+package com.blz.example.model;
+
+import com.blz.example.dto.EPADTO;
+
+import lombok.Data;
+
+@Data
+public class EPAData {
+
+	private int employeeId;
+	private String name;
+	private long salary;
+
+	public EPAData() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public EPAData(int employeeId, String name, long salary) {
+		super();
+		this.employeeId = employeeId;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public EPAData(int employeeId, EPADTO employeePayrollDTO) {
+		super();
+		this.employeeId = employeeId;
+		this.name = employeePayrollDTO.name;
+		this.salary = employeePayrollDTO.salary;
+	}
+}
