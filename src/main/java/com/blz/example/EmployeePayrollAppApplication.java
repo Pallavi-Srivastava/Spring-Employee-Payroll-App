7485b466b93412f7dc8424a7f6e38953cabd2cd4
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-12-22 18:42:33 +0530

    Added:Code to add validation to name field so the REST call can be validated.

---
+++ src/main/java/com/blz/example/EmployeePayrollAppApplication.java
@@ -0,0 +1,17 @@
+package com.blz.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class EmployeePayrollAppApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context= SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		log.info("Employee Payroll App started in {} environment",context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll DB User is {} ",context.getEnvironment().getProperty("spring.datasource.username"));
+	}
+}
