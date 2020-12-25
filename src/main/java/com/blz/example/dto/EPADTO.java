60a35d0b41be3eb1d6200baecbf469b5d650e046
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-12-25 23:56:12 +0530

    Added:Code to add remaining properties to EPA dto and model.

---
+++ src/main/java/com/blz/example/dto/EPADTO.java
@@ -0,0 +1,32 @@
+package com.blz.example.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.ToString;
+
+@ToString
+public class EPADTO {
+
+	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Invalid Employee Name")
+	@NotEmpty(message = "Employee name cannot be null ")
+	public String name;
+
+	@Min(value = 5000, message = "Min wage should be more than 5000 ")
+	public long salary;
+
+	public String gender;
+
+	@JsonFormat(pattern = "dd MMM yyyy")
+	public LocalDate startDate;
+
+	public String note;
+
+	public String profilePic;
+
+	public List<String> departments;
+
+}
---
+++ src/test/java/com/blz/example/EmployeePayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blz.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
